50aba210766e7b2f6e1b87fd67654dd02d147ce6
guilherme <devguilhermeribeiro000@gmail.com>
2025-10-13 11:36:09 -0300

    Adding login handler

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Models\Job;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\View\View;
 
 class JobController extends Controller
@@ -14,4 +17,20 @@ class JobController extends Controller
         ]);
     }
 
+    public function login(Request $request): RedirectResponse
+    {
+        $credentials = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+            return redirect()->route("jobs");
+        }
+
+        return back()->withErrors([
+            'email' => 'Credenciais inválidas'
+        ])->onlyInput("email");
+    }
 }
