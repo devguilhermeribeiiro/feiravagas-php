60fd1c63ea05efd12ec0388d12d0321c74299042
guilherme <devguilhermeribeiro000@gmail.com>
2025-10-14 09:58:02 -0300

    Simplify style

--- resources/views/components/jobs/dialog.blade.php
+++ resources/views/components/jobs/dialog.blade.php
@@ -1,5 +1,5 @@
 <dialog id="{{ $target }}-modal" class="modal {{$target === "publicar" ? "md:h-11/12" : "md:h-4/5"}}">
-    <section class="flex flex-col px-4 py-10 mx-auto gap-4 items-center justify-between h-full">
+    <section class="px-4 py-10 mx-auto h-full">
 
         <button id="btn-{{$target}}-modal" class="absolute top-4 right-4 text-xl lg:text-2xl text-slate-600 hover:text-white cursor-pointer font-bold px-2 hover:rounded hover:bg-slate-700 transition">
             &times;
